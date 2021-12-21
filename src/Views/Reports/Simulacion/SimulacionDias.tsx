cbb6e98c87b805bfc159a961f88cba5dfd69f882
Joel Valencia <joelvalenciavila@gmail.com>
2021-12-21 12:18:01 -0400

    test

--- src/Views/Reports/Simulacion/SimulacionDias.tsx
+++ src/Views/Reports/Simulacion/SimulacionDias.tsx
@@ -67,7 +67,7 @@ const SimulacionDias = () => {
 					/>
 				</Col>
 			</Col>
-			<Col sm={12} className='mt-4'>
+			<Col sm={12} className='mt-3'>
 				{((loading == false) && (errorMessageModule.length === 0))
 					? simulacionGrafica 
 					: <ShowMessageInModule message = {errorMessageModule}/> 
