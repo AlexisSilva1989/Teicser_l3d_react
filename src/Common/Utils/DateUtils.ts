bfafac318f1528db39bfae5173c324c1b6b26283
Joel Valencia <joelvalenciavila@gmail.com>
2023-05-30 19:13:39 -0400

    CAMBIAR FORMATO DE FECHA

--- src/Common/Utils/DateUtils.ts
+++ src/Common/Utils/DateUtils.ts
@@ -22,6 +22,6 @@ export class DateUtils {
     const year = date.getFullYear().toString().padStart(4, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
-    return `${day}/${month}/${year}`;
+    return `${year}-${month}-${day}`;
   }
 }
